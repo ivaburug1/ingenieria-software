4518a691ae44427e978721a5fb612f7b374b7d7c
Ivan Urso <ivanalejandrourso@gmail.com>
2025-12-03 16:16:33 -0300

    03/12/2025 - 16:16

---
+++ nuevo/Programacion/StageLink_02-12-2025/BLL_391IAU/BLLPermiso.cs
@@ -0,0 +1,19 @@
+﻿using DAL_391IAU;
+using BE_391IAU;
+using System.Collections.Generic;
+
+namespace BLL_391IAU
+{
+    public class BLLPermiso
+    {
+        public List<BEPermiso> TraerPermisosSimples()
+        {
+            return DAL.ObtenerTodosLosPermisosSimples();
+        }
+
+        public BEPermiso TraerPermiso(int id)
+        {
+            return DAL.ObtenerPermisoPorID(id);
+        }
+    }
+}
---
+++ nuevo/Programacion/StageLink_02-12-2025/BLL_391IAU/BLlPerfil.cs
@@ -0,0 +1,28 @@
+﻿using BE_391IAU;
+using DAL_391IAU;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_391IAU
+{
+    public class BLLPerfil
+    {
+        public BEPerfil TraerPerfil(int idPerfil)
+        {
+            return DAL.ObtenerPerfilPorID(idPerfil);
+        }
+
+        public List<BEPermiso> ObtenerPermisosSimples(int idPerfil)
+        {
+            return DAL.ObtenerPermisosDePerfil(idPerfil);
+        }
+
+        public List<BEPerfil> TraerPerfiles()
+        {
+            return DAL.ObtenerPerfiles();
+        }
+    }
+}
